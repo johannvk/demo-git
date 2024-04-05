1821b3dd044544488d54b5e4b6fdf0bbb82370d8
jvkolsto <jvkolsto@nr.no>
2024-04-05 14:25:08 +0200

    Added second model, and said hello to everybody.

--- src/demo/__init__.py
+++ src/demo/__init__.py
@@ -1,7 +1,12 @@
 """This file tells Python that the directory is a package."""
 
 def main():
-    print("Hello, World!")
+    print("Hello, everybody here!")
+
+def second_model():
+    print("This is the second model.")
+    x = 2 
+    return x**2
 
 if __name__ == "__main__":
     main()
