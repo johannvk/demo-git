c6795aa6129a2c305622c1331b90d5429354ff21
jvkolsto <jvkolsto@nr.no>
2024-05-13 15:25:07 +0200

    Minor changes, project description

--- src/demo/modeling.py
+++ src/demo/modeling.py
@@ -1,8 +1,8 @@
-# import ERApy as era
+import ERApy as era
 
 
-# def era_data_directory():
-#     era_dir = era.ERA_dir()
-#     return f"{era_dir}/01-04-2024.csv"
+def era_data_directory():
+    era_dir = era.ERA_dir()
+    return f"{era_dir}/01-04-2024.csv"
 
 
